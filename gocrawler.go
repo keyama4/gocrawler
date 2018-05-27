275ed892f65e9745c270ffd5af5c8e55ea2f8805
Yosuke Hatakeyama <yosuke.hatakeyama@graffer.jp>
2018-05-27 17:33:59 +0900

    Apply variable length arguments

--- gocrawler.go
+++ gocrawler.go
@@ -6,16 +6,8 @@ import (
 )
 
 // CrawlOnChrome is crawling with common process on chrome driver
-func CrawlOnChrome(callback func(page *agouti.Page)) {
-	crawl(callback, agouti.ChromeDriver())
-}
-
-// CrawlOnPhantom is crawling with common process on PhantomJS
-func CrawlOnPhantom(callback func(page *agouti.Page)) {
-	crawl(callback, agouti.PhantomJS())
-}
-
-func crawl(callback func(page *agouti.Page), driver *agouti.WebDriver) {
+func CrawlOnChrome(callbacks ...func(page *agouti.Page)) {
+	driver := agouti.ChromeDriver()
 	if err := driver.Start(); err != nil {
 		log.Fatalf("Failed to start driver:%v", err)
 	}
@@ -24,5 +16,23 @@ func crawl(callback func(page *agouti.Page), driver *agouti.WebDriver) {
 	if err != nil {
 		log.Fatalf("Failed to open page:%v", err)
 	}
-	callback(page)
+	for _, callback := range callbacks {
+		callback(page)
+	}
+}
+
+// CrawlOnPhantom is crawling with common process on PhantomJS
+func CrawlOnPhantom(callbacks ...func(page *agouti.Page)) {
+	driver := agouti.PhantomJS()
+	if err := driver.Start(); err != nil {
+		log.Fatalf("Failed to start driver:%v", err)
+	}
+	defer driver.Stop()
+	page, err := driver.NewPage()
+	if err != nil {
+		log.Fatalf("Failed to open page:%v", err)
+	}
+	for _, callback := range callbacks {
+		callback(page)
+	}
 }
